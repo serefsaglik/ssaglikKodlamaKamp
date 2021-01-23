b33fa3306cfd58a00867ae673a399fc98bc464e8
ssaglik <serefsaglik15@gmail.com>
2021-01-23 23:55:18 +0300

    Add project files.

---
+++ Koleksiyonlar/List.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    internal class List
+    {
+        public static implicit operator List(List<string> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method injection - alt satırda sagda IloggerService logger service olarak yapılan uygulama
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            //Başvuran bilgilerini değerlendirme
+            //
+            //KonutKrediManager konutKrediManager = new KonutKrediManager(); //bütün başvurular konut
+            //konutKrediManager.Hesapla(); //kredimanager üzerinden yapılır. dogru bir uygulama değil.
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+
+    }
+}
